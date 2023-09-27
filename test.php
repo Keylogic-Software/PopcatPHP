08885c1b7a27b943a93d407cfaddd1d89252b904
Givinghawk <giveinghawk@gmail.com>
2023-09-27 16:57:59 +0100

    Tests

--- test.php
+++ test.php
@@ -1,11 +1,7 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-// do not use in production, tests only
-//require __DIR__ . '/src/Popcatwrapper.php';
-
-use Keylogic\Popcatwrapper\Main;
-
 $popcat = new Keylogic\Popcatwrapper\Main();
 
-echo $popcat->drake('gay', "gay");
+$popcat->element('He');
+$popcat->imdb('The bee movie');
