e3eb3e2ec36d0e475f39843eeb2991d746c9408b
Bananonz <70526508+Bananonz@users.noreply.github.com>
2024-03-30 23:28:37 +0100

    Test fix - part 2

--- test.php
+++ test.php
@@ -3,8 +3,23 @@ require __DIR__ . '/vendor/autoload.php';
 
 $popcat = new Keylogic\Popcatwrapper\Main();
 
-echo $popcat->element('He') . PHP_EOL;
-echo $popcat->imdb('The bee movie') . PHP_EOL;
-echo $popcat->screenshot('https://google.com') . PHP_EOL;
+$elementResult = $popcat->element('He');
+if (isset($elementResult->name)) {
+    echo "Element Name: " . $elementResult->name . "\n";
+    echo "Element Symbol: " . $elementResult->symbol . "\n";
+} else {
+    echo "Error: " . $elementResult->error . "\n";
+}
+
+$imdbResult = $popcat->imdb('The bee movie');
+if (isset($imdbResult->Title)) {
+    echo "Movie Title: " . $imdbResult->Title . "\n";
+    echo "IMDB Rating: " . $imdbResult->imdbRating . "\n";
+} else {
+    echo "Error: " . $imdbResult->error . "\n";
+}
+
+echo $popcat->screenshot('https://google.com');
+
 echo 'Tests completed!';
 ?>
