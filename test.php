8910b48795a685a708ab4f944e787bdf2b1d8ef5
Bananonz <70526508+Bananonz@users.noreply.github.com>
2024-03-30 23:25:33 +0100

    Test fixes - firs try?

--- test.php
+++ test.php
@@ -3,7 +3,8 @@ require __DIR__ . '/vendor/autoload.php';
 
 $popcat = new Keylogic\Popcatwrapper\Main();
 
-echo $popcat->element('He');
-echo $popcat->imdb('The bee movie');
-echo $popcat->screenshot('https://google.com');
+echo $popcat->element('He') . PHP_EOL;
+echo $popcat->imdb('The bee movie') . PHP_EOL;
+echo $popcat->screenshot('https://google.com') . PHP_EOL;
 echo 'Tests completed!';
+?>
